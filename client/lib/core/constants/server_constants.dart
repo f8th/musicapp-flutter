915f446c1a18db193badc6b287b3d3c00ec21f7d
Anonymous3017 <ayushgupta9592@gmail.com>
2025-04-10 18:17:47 +0530

    changes the server url

--- client/lib/core/constants/server_constants.dart
+++ client/lib/core/constants/server_constants.dart
@@ -1,5 +1,10 @@
 import 'dart:io';
 
+// class ServerConstants {
+//   static String baseUrl = Platform.isAndroid ? 'http://10.0.2.2:8000' : 'http://localhost:8000';
+// }
+
 class ServerConstants {
-  static String baseUrl = Platform.isAndroid ? 'http://10.0.2.2:8000' : 'http://localhost:8000';
+  static String baseUrl =
+      Platform.isAndroid ? 'http://3.111.38.197' : 'http://3.111.38.197';
 }
